5b0e2f50e9aa3ed813500236ea8b4b1f94732acb
Andrus Adamchik <andrus@objectstyle.com>
2020-08-22 14:27:06 +0300

    javadocs

--- src/main/java/org/objectstyle/flow/FlowVisitor.java
+++ src/main/java/org/objectstyle/flow/FlowVisitor.java
@@ -1,14 +1,16 @@
 package org.objectstyle.flow;
 
 /**
- * Allows to traverse the flow
+ * Allows to traverse the flow tree depth-first. 
+ * 
+ * @see Flow#accept(FlowVisitor) 
  */
 public interface FlowVisitor {
 
     /**
      * A visitor callback method invoked on every flow node.
      *
-     * @return true if the visitor wants to to continue traversal of this node's children, false - if traversal
+     * @return true if the visitor wants to continue traversal of this node's children, false - if traversal
      * should stop at this branch and move on to peer nodes.
      */
     boolean onFlowNode(Flow node, FlowPath path);
