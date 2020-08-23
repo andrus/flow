25357377f0635affa9517cc50edac6ee873249d3
Andrus Adamchik <andrus@objectstyle.com>
2020-08-23 13:37:08 +0300

    docs

--- src/main/java/org/objectstyle/flow/op/InsertOp.java
+++ src/main/java/org/objectstyle/flow/op/InsertOp.java
@@ -7,6 +7,7 @@ import org.objectstyle.flow.StepProcessor;
 public class InsertOp extends ReplaceOp {
 
     public InsertOp(Flow flow, FlowPath insertAt, StepProcessor<?> toInsert) {
+        // create a flow with no egresses.. Default egress will be added at the insertion point
         super(flow, insertAt, Flow.of(toInsert));
     }
 
