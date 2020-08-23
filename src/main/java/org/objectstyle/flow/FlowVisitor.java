16c4a968274bd4b4650d52112a84d02e221be52c
Andrus Adamchik <andrus@objectstyle.com>
2020-08-23 11:03:35 +0300

    javadocs

--- src/main/java/org/objectstyle/flow/FlowVisitor.java
+++ src/main/java/org/objectstyle/flow/FlowVisitor.java
@@ -8,13 +8,23 @@ package org.objectstyle.flow;
 public interface FlowVisitor {
 
     /**
-     * A visitor callback method invoked on every flow node.
+     * A visitor callback method invoked before each flow node.
      *
+     * @param path path to the node from the traversal root
+     * @param node visited node
      * @return true if the visitor wants to continue traversal of this node's children, false - if traversal
-     * should stop at this branch and move on to peer nodes.
+     * should stop at this branch and move on to peer nodes. Note that on "false" matching
+     * {@link #afterNode(FlowPath, Flow)} will not be called.
      */
     boolean beforeNode(FlowPath path, Flow node);
 
+    /**
+     * Invoked after the node and all its children are processed. Not invoked if the matching
+     * {@link #beforeNode(FlowPath, Flow)} returned false.
+     *
+     * @param path path to the node from the traversal root
+     * @param node visited node
+     */
     default void afterNode(FlowPath path, Flow node) {
     }
 }
